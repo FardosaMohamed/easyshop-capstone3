c40ed3145d3afcf5f8e2cc3d9cd2c36d412b08bd
fardosamohamed <Fardosa.Mohamed2@seattlecolleges.edu>
2025-06-25 13:35:54 -0700

    Integrated JdbcTemplate into MySqlDaoBase
    - Centralized JdbcTemplate initialization in MySqlDaoBase. - This provides
    a shared JdbcTemplate instance for all extending DAO classes, simplifying
    their constructors and promoting consistency.

--- src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
+++ src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
@@ -1,16 +1,20 @@
 package org.yearup.data.mysql;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public abstract class MySqlDaoBase
 {
-    private DataSource dataSource;
+    protected final JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
 
     public MySqlDaoBase(DataSource dataSource)
     {
         this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     protected Connection getConnection() throws SQLException
